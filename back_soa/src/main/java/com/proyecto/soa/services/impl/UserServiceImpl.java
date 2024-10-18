b3bf60b0991a0993a34ddd65014118e0abfaed0c
ErickCR2003 <carranzaerick96@gmail.com>
2024-10-18 01:19:56 -0500

    userUpdate en capas

--- back_soa/src/main/java/com/proyecto/soa/services/impl/UserServiceImpl.java
+++ back_soa/src/main/java/com/proyecto/soa/services/impl/UserServiceImpl.java
@@ -6,6 +6,9 @@ import com.proyecto.soa.model.entities.User;
 import com.proyecto.soa.repositories.RolRepository;
 import com.proyecto.soa.repositories.UserRepository;
 import com.proyecto.soa.services.UserService;
+import com.proyecto.soa.validation.UserValid;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,22 +19,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
+//@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private UserRepository userRepository;
-    private RolRepository rolRepository;
-    private PasswordEncoder passwordEncoder;
+
+    private final UserValid userValid;
+    private final UserRepository userRepository;
+    private final RolRepository rolRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+
+    public UserServiceImpl(UserValid userValid, UserRepository userRepository, RolRepository rolRepository, PasswordEncoder passwordEncoder, ModelMapper modelMapper) {
+        this.userValid = userValid;
+        this.userRepository = userRepository;
+        this.rolRepository = rolRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
+    }
 
     //Se injecta las dependencias
-    public UserServiceImpl(
-            UserRepository userRepository,
-            RolRepository rolRepository,
-            PasswordEncoder passwordEncoder
-    ){
-        this.userRepository=userRepository;
-        this.rolRepository=rolRepository;
-        this.passwordEncoder=passwordEncoder;
-    }
 
     @Transactional(readOnly = true)
     @Override
@@ -62,20 +69,10 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public Optional<User> update(UserUpdateDTO user, Long id) {
-        Optional<User> userOptional=userRepository.findById(id);
-
-        if(userOptional.isPresent()){
-            //se inicializa el objeto userUpdate con el objeto userOptional
-            User userUpdate=userOptional.get();
-            userUpdate.setName(user.getName());
-            userUpdate.setLastname(user.getLastname());
-            userUpdate.setEmail(user.getEmail());
-//            userUpdate.setRoles(getRoles(user));
-            //se retorna el objeto userUpdate
-            return Optional.of(userRepository.save(userUpdate));
-        }
-        //se retorna un objeto vacio
-        return Optional.empty();
+        userValid.validUser(id);
+        User userUpdate = userRepository.findById(id).orElse(null);
+        modelMapper.map(user,userUpdate);
+        return Optional.of(userRepository.save(userUpdate));
     }
 
 //    private List<Role> getRoles(IUser user){
