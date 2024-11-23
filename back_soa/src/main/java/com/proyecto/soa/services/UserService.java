3066f42f02586cbe66f6cedc4e6c90676253dfcc
RonaldX7 <ronaldjair0705@gmail.com>
2024-11-23 16:48:14 -0500

    nuevos cambios

--- back_soa/src/main/java/com/proyecto/soa/services/UserService.java
+++ back_soa/src/main/java/com/proyecto/soa/services/UserService.java
@@ -6,9 +6,12 @@ import com.proyecto.soa.model.dtos.UserUpdateRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface UserService {
     Page<UserResponse> findAll(Pageable pageable);
     UserResponse findById(Long id);
+    List<UserResponse> findByEmail(String email);
     UserResponse save(UserCreateRequest user);
     void deleteById(Long id);
     UserResponse update(UserUpdateRequest user, Long id);
