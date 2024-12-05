bc97f469472dcd1ec2a3a068e16bcf138f20786e
manumaybe <manu.alferez.castro@gmail.com>
2024-12-04 21:34:22 -0500

    nuevos cambios creacion de grupos

--- back_soa/src/main/java/com/proyecto/soa/repositories/UserRepository.java
+++ back_soa/src/main/java/com/proyecto/soa/repositories/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -25,5 +27,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
     void updatePassword(String password, Long userId);
 
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.id IN :ids")
+    List<User> findUsersByIds(@Param("ids") List<Long> ids);
 
 }
