84a87ba672f67eecbd9f1d8d2932d211ea18c23f
Bruno Sandoval <bruno.sandoval210@gmail.com>
2024-12-07 11:12:57 -0500

    se implemento un servicio para obtener obtener las columnas de una tabla

---
+++ back_soa/src/main/java/com/proyecto/soa/model/dtos/ColumnsTableResponse.java
@@ -0,0 +1,11 @@
+package com.proyecto.soa.model.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ColumnsTableResponse {
+    private Long id;
+    private String name;
+}
